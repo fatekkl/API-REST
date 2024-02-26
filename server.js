7e50de9f3f560876acd92707c6f528de594a3e22
fatekkl <mathtml.1105@gmail.comm>
2024-02-26 09:12:35 -0300

    resolving ENOENT

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const routes = require("./routes.js")
+
+const PORT = 3030
+
+
+app.set("port" , process.env.PORT)
+
+app.use(express.json());
+
+
+routes.forEach(route => {
+    app[route.method.toLowerCase()](route.path , route.handler)
+});
+
+
+
+app.listen(PORT , () => {
+    console.log(`Server open in ${PORT}`)
+})
