b3c89e7bb406d00e585826ef31b58b9d97abf7ef
fatekkl <mathtml.1105@gmail.comm>
2024-04-11 13:55:24 -0300

    testing features

--- api/cripto.js
+++ api/cripto.js
@@ -12,4 +12,3 @@ export default function salhash(senha) {
 
     return {hash , sal};
 }
-
--- api/server.js
+++ api/server.js
@@ -4,23 +4,20 @@ const app = Express()
 
 import { routes } from "./routes.js"
 
-
-
-
 const PORT = 3030
 
 
-app.set("port" , process.env.PORT)
+app.set("port", process.env.PORT)
 
 app.use(Express.json());
 
 
 routes.forEach(route => {
-    app[route.method.toLowerCase()](route.path , route.handler)
+    app[route.method.toLowerCase()](route.path, route.handler)
 });
 
 
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
     console.log(`Server open in ${PORT}`)
 })
